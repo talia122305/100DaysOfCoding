3bad6bfd11bdfa092f6f1c961facfad5e61a090a
talia122305 <desinataliaa.0023@gmail.com>
2025-09-10 18:51:44 +0800

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,20 @@
+public class ContohKomentar {
+    public static void main(String[] args) {
+        
+        // Menampilkan teks ke layar
+        System.out.println("Halo, dunia!");
+
+        // Menjumlahkan dua angka
+        int a = 5;
+        int b = 3;
+        int hasil = a + b;
+
+        // Menampilkan hasil penjumlahan
+        System.out.println("Hasil: " + hasil);
+
+        /*
+         * Ini adalah komentar multi-baris.
+         * Bisa digunakan untuk menjelaskan bagian kode yang lebih panjang.
+         */
+    }
+}
