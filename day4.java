f944e7742695729d0f312780c1c8833b0eb95dc1
talia122305 <desinataliaa.0023@gmail.com>
2025-09-11 17:48:16 +0800

    day4.java

---
+++ day4.java
@@ -0,0 +1,9 @@
+public class Day4 {
+    public static void main(String[] args) {
+        System.out.println("Halo,\nDunia!");                     // \n = baris baru
+        System.out.println("Nama:\tDesi Natalia");               // \t = tab
+        System.out.println("Dia berkata: \"Halo, saya Desi!\""); // \" = tanda kutip
+        System.out.println("Folder: C:\\Users\\Desi\\Dokumen");  // \\ = backslash
+        System.out.println("Simbol hati: \u2665");               // Unicode simbol hati
+    }
+}
