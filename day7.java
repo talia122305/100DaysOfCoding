6043c851ee90096f14decba69a8972d3c1c8d736
talia122305 <desinataliaa.0023@gmail.com>
2025-09-14 19:24:21 +0800

    day7.java

---
+++ day7.java
@@ -0,0 +1,9 @@
+public class Day7 {
+    public static void main(String[] args) {
+        char huruf = 'A';
+        boolean isCapital = (huruf >= 'A' && huruf <= 'Z');
+
+        System.out.println("Huruf: " + huruf);
+        System.out.println("Apakah huruf kapital? " + isCapital);
+    }
+}
