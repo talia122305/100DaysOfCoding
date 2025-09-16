1a0ab0c2d9982560ef8ef4ba2a9eaf7783375a51
talia122305 <desinataliaa.0023@gmail.com>
2025-09-16 20:11:23 +0800

    day9.java

---
+++ day9.java
@@ -0,0 +1,12 @@
+public class Day9 {
+    public static void main(String[] args) {
+        // Deklarasi dan inisialisasi variabel
+        int nilai = 20;
+        System.out.println("Nilai awal: " + nilai);
+
+        // Update variabel
+        nilai = nilai + 10;
+        System.out.println("Nilai setelah update: " + nilai);
+    }
+}
+
