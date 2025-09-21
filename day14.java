baa9655abe9da4f52b22c09f778b4eccece59e41
talia122305 <desinataliaa.0023@gmail.com>
2025-09-21 15:55:36 +0800

    day14.java

---
+++ day14.java
@@ -0,0 +1,15 @@
+public class lia {
+
+    public static void main(String[] args) {
+        String sInt = "50";
+        int a = Integer.parseInt(sInt);
+        String sDouble = "4.14";
+        double b= Double.parseDouble(sDouble);
+        String sBool = "true";
+        boolean d= Boolean.parseBoolean(sBool);
+
+        System.out.println("int: " + a);
+        System.out.println("double: " + b);
+        System.out.println("boolean: " + d);
+    }
+}
