a43245dbee01b5e5fda71ef6fe0689fa92e05ed9
talia122305 <desinataliaa.0023@gmail.com>
2025-09-22 17:25:54 +0800

    day15.java

---
+++ day15.java
@@ -0,0 +1,15 @@
+public class day15 {
+    public static void main(String[] args) {
+        double angka1 = 20.5;
+        double angka2 = 10.5;
+
+        // operasi penjumlahan
+        double hasilpenjumlahan = angka1+angka2;
+        System.out.println(("hasil penjumlahan :"  +hasilpenjumlahan));
+
+
+        // oeprasi pengurangan
+        double hasilpengurangan = angka1-angka2;
+        System.out.println("hasil pengurangan :" + hasilpengurangan);
+    }
+}
