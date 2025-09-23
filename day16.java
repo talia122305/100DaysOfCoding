021385ed64e11ff096e535a8299ecb46ae7932df
talia122305 <desinataliaa.0023@gmail.com>
2025-09-23 15:19:35 +0800

    day16.java

---
+++ day16.java
@@ -0,0 +1,10 @@
+public class day16 {
+    public static void main(String[] args) {
+            int a = 20;
+             int b = 4;
+        // menampilkan hasil
+        System.out.println("hasilk perkalian:"+ a*b);
+        System.out.println("hasil pembagi:"+a/b);
+        
+    }
+}
