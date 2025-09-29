2f6841e8d30b22d195622d1378cdc14b4eb72db4
talia122305 <desinataliaa.0023@gmail.com>
2025-09-29 19:45:20 +0800

    day22.java

---
+++ day22.java
@@ -0,0 +1,9 @@
+public class day22 {
+    public static void main(String[] args) {
+        int sisi =10;
+        //menghitung luas
+        int luas =sisi*sisi;
+        System.out.println("sisi persegi="+sisi);
+        System.out.println("luas persegi="+luas);
+    }
+}
