914a1dd4c8a83a579e6b4f445f1101f46124c18b
talia122305 <desinataliaa.0023@gmail.com>
2025-09-30 19:07:03 +0800

    day23.java

---
+++ day23.java
@@ -0,0 +1,7 @@
+public class day23 {
+    public static void main(String[] args) {
+        int panjang =20;
+        int lebar =10;
+        System.out.println("luas persegi panjang:"+panjang*lebar);
+    }
+}
