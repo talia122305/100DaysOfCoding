4851e6b87f22da775dea12baf4a63756690becac
talia122305 <desinataliaa.0023@gmail.com>
2025-10-14 19:23:24 +0800

    day32.java

---
+++ day32.java
@@ -0,0 +1,13 @@
+public class Day32 {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 12;
+
+        // Operator logika OR tanpa if
+        boolean hasil = (a > 8) || (b > 10);
+
+        System.out.println("Nilai a = " + a);
+        System.out.println("Nilai b = " + b);
+        System.out.println("Hasil dari (a > 8 atau b > 10) = " + hasil);
+    }
+}
