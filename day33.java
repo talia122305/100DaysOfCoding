79683192a79d1263e5028916fcaa10a6ce98ed43
talia122305 <desinataliaa.0023@gmail.com>
2025-10-14 19:25:02 +0800

    day33.java

---
+++ day33.java
@@ -0,0 +1,9 @@
+public class Day33 {
+    public static void main(String[] args) {
+        
+        boolean nyala = true;
+
+        System.out.println(nyala);    // true
+        System.out.println(!nyala);   // false
+    }
+}
