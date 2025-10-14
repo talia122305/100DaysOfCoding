b4c71c9b82d8496c0e80a2c2ff87ad471cd7106b
talia122305 <desinataliaa.0023@gmail.com>
2025-10-14 19:04:09 +0800

    day24.java

---
+++ day24.java
@@ -0,0 +1,8 @@
+public class day24 {
+    public static void main(String[] args) {
+        double jari =9;
+        double luas =3.14*jari*jari;
+        System.out.println("jari -jari lingkaran:"+jari);//hasilnya 9.0
+        System.out.println("luas lingkaran:"+luas);// hasilnya 254.34
+    }
+}
