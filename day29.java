56339f3e481608bfb31b98d25e9889bd60692793
talia122305 <desinataliaa.0023@gmail.com>
2025-10-14 19:14:57 +0800

    day29.java

---
+++ day29.java
@@ -0,0 +1,8 @@
+public class day29 {
+    public static void main(String[] args) {
+        int c =7;
+        int d = 8;
+        System.out.println("c<d="+(c<d));
+        System.out.println("c>d="+(c>d));
+    }
+}
