7e8e7049435e13570476281af3cfcc164da0621e
talia122305 <desinataliaa.0023@gmail.com>
2025-10-14 19:05:09 +0800

    day25.java

---
+++ day25.java
@@ -0,0 +1,13 @@
+public class day25 {
+    public static void main(String[] args) {
+                System.out.println("Perulangan Increment:");
+                for (int i = 10; i <= 15; i++) {
+                    System.out.println("i = " + i);
+                }
+
+                System.out.println("Perulangan Decrement:");
+                for (int j = 15; j >= 10; j--) {
+                    System.out.println("j = " + j);
+                }
+            }
+        }
