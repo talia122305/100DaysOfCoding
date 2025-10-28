4a2a773fe2d5aee3c72257959553cc055064dae9
talia122305 <desinataliaa.0023@gmail.com>
2025-10-28 21:23:36 +0800

    day51.java

---
+++ day51.java
@@ -0,0 +1,13 @@
+
+    public class day51 {
+        public static void main(String[] args) {
+            System.out.println("=== Program Perulangan For ===");
+          
+            for (int i = 1; i <= 2; i++) {
+                System.out.println("Angka ke-" + i);
+            }
+
+            System.out.println("=== Selesai ===");
+        }
+    }
+
