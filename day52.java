1be31f941606c698178923f46fda93ac89c4190f
talia122305 <desinataliaa.0023@gmail.com>
2025-10-29 21:05:40 +0800

    day52.java

---
+++ day52.java
@@ -0,0 +1,12 @@
+public class day52 {
+    public static void main(String[] args) {
+        int i = 1; // nilai awal
+        System.out.println("=== Program Perulangan While ===");
+        // perulangan while
+        while (i <= 5) {
+            System.out.println("Angka ke-" + i);
+            i++; // menambah nilai i
+        }
+        System.out.println("Perulangan selesai!");
+    }
+}
