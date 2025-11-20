a5e11ae4e4ebf06efc07e62f8407698a4750bb86
talia122305 <desinataliaa.0023@gmail.com>
2025-11-20 17:44:30 +0800

    day74.java

---
+++ day74.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class soal1 {
+
+ public static void main(String[] args) {
+ Scanner sn = new Scanner(System .in);
+   int angka = 1;
+
+  do {
+   if (angka == 50) {
+    continue;
+   }
+   if (angka == 98) {
+    break;
+   }
+
+  } while (angka < 100) ;
+
+
+}
+
+}
+
