4ad50a9e27fa4a0e3219c8b25407685a8955632e
talia122305 <desinataliaa.0023@gmail.com>
2025-11-21 16:17:48 +0800

    day75.java

---
+++ day75.java
@@ -0,0 +1,11 @@
+public class Day75Method {
+    public static void main(String[] args) {
+
+        String teks = "Belajar Java";
+
+        System.out.println("Teks asli   : " + teks);
+        System.out.println("Panjang     : " + teks.length());
+        System.out.println("Huruf besar : " + teks.toUpperCase());
+        System.out.println("Huruf kecil : " + teks.toLowerCase());
+    }
+}
